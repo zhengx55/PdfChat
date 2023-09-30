8e75f81cb38cec071e302abe6656f4373730ffee
zhengx55 <williamzhxin@gmail.com>
2023-09-30 06:59:54 -0400

    config

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
+
+  //   const dbUser = await db.user.findFirst({
+  //     where: {
+  //       id: user.id,
+  //     },
+  //   });
+
+  //   if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  //   const subscriptionPlan = await getUserSubscriptionPlan();
+
+  return <main>{user.email}</main>;
+};
+
+export default Page;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
 
 type Props = {};
 
@@ -10,7 +13,37 @@ const Navbar = (props: Props) => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           {/* logo */}
-          <Link href={'/'} className="flex z-40 font-semibold"></Link>
+          <Link href={"/"} className="flex z-40 font-semibold">
+            <span>App.</span>
+          </Link>
+          <div className="hidden items-center space-x-4 sm:flex">
+            <>
+              <Link
+                href={"/pricing"}
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Pricing
+              </Link>
+              <LoginLink
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "sm",
+                })}
+              >
+                Sign in
+              </LoginLink>
+              <RegisterLink
+                className={buttonVariants({
+                  size: "sm",
+                  })}
+              >
+                Sign up <ArrowRight className="ml-1.5 w-5 h-5" />
+              </RegisterLink>
+            </>
+          </div>
         </div>
       </MaxWidthWrapper>
     </nav>
